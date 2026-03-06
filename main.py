ab3097813dc7d7aab833927ed28a43e4ce7c8c45
ssh1336 <lalualwazir534@gmail.com>
2026-03-06 06:27:18 +0300

    Update bot token to placeholder
    Replaced the actual bot token with a placeholder.

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import telebot
 from telebot.types import ChatPermissions
 import time
 
-TOKEN = "8737030720:AAElxZsukpOKGyakWCfhXaRJfCih85nduMQ"
+TOKEN = "BOT_TOKEN"
 bot = telebot.TeleBot(TOKEN)
 
 print("🔥 Global Bot V2 is running...")
